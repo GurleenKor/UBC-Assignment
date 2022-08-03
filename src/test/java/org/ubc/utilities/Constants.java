5580d060a496766d1c563f56ea2b2540c61afedb
Gurleen Kaur <gurleenkaurdhawan@gmail.com>
2022-08-03 03:11:06 -0700

    Token Updated

--- src/test/java/org/ubc/utilities/Constants.java
+++ src/test/java/org/ubc/utilities/Constants.java
@@ -2,5 +2,5 @@ package org.ubc.utilities;
 
 public interface Constants {
     String URL = "https://api.github.com/gists";
-    String TOKEN = "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";
+    String TOKEN = "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";
 }
